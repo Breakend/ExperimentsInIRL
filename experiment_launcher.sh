4a4a81adc06d85924f31b4a964a58dfdb5d3aa2d
Peter Henderson <petehend@cim.mcgill.ca>
2017-04-18 17:15:03 -0400

    fix experiment launcher for testing

--- experiment_launcher.sh
+++ experiment_launcher.sh
@@ -6,34 +6,33 @@
 
 # 4 frames, importance 0
 
-cmd="python -u run_training_gans.py experts/expert_rollouts_CartPole-v0.pickle novices/novice_policy_CartPole_gan_mix_f4.o --num_frames 4 --num_experiments 5 --importance_weights 0.0 --algorithm mixgan --iterations 40 --env CartPole-v0 > mix_log 2> mix_error_log"
-disown -h $cmd &
+cmd="python -u run_training_gans.py experts/expert_rollouts_CartPole-v0.pickle novices/novice_policy_CartPole_gan_mix_f4.o --num_frames 4 --num_experiments 5 --importance_weights 0.0 --algorithm mixgan --iterations 40 --env CartPole-v0"
+nohup $cmd &> mix_log_1 &
 
-cmd="python -u run_training_gans.py experts/expert_rollouts_CartPole-v0.pickle novices/novice_policy_CartPole_gan_opt_f4.o --num_frames 4 --num_experiments 5 --importance_weights 0.0 --algorithm optiongan --iterations 40 --env CartPole-v0 > opt_log 2> opt_error_log"
-disown -h $cmd &
+cmd="python -u run_training_gans.py experts/expert_rollouts_CartPole-v0.pickle novices/novice_policy_CartPole_gan_opt_f4.o --num_frames 4 --num_experiments 5 --importance_weights 0.0 --algorithm optiongan --iterations 40 --env CartPole-v0"
+nohup $cmd &> op_log_1 &
 
-cmd="python -u run_training_gans.py experts/expert_rollouts_CartPole-v0.pickle novices/novice_policy_CartPole_gan_rlgan_f4.o --num_frames 4 --num_experiments 5 --importance_weights 0.0 --algorithm rlgan --iterations 40 --env CartPole-v0 > gan_log 2> gan_error_log"
-
-$cmd
+cmd="python -u run_training_gans.py experts/expert_rollouts_CartPole-v0.pickle novices/novice_policy_CartPole_gan_rlgan_f4.o --num_frames 4 --num_experiments 5 --importance_weights 0.0 --algorithm rlgan --iterations 40 --env CartPole-v0"
+eval $cmd &> ganlog
 
 # 4 frames importance .1
 
-cmd="python -u run_training_gans.py experts/expert_rollouts_CartPole-v0.pickle novices/novice_policy_CartPole_gan_mix_f4.o --num_frames 4 --num_experiments 5 --importance_weights 0.1 --algorithm mixgan --iterations 40 --env CartPole-v0 > mix_log 2> mix_error_log"
-disown -h $cmd &
+#cmd="python -u run_training_gans.py experts/expert_rollouts_CartPole-v0.pickle novices/novice_policy_CartPole_gan_mix_f4.o --num_frames 4 --num_experiments 5 --importance_weights 0.1 --algorithm mixgan --iterations 40 --env CartPole-v0 > mix_log 2> mix_error_log"
+#nohup  $cmd &
 
-cmd="python -u run_training_gans.py experts/expert_rollouts_CartPole-v0.pickle novices/novice_policy_CartPole_gan_opt_f4.o --num_frames 4 --num_experiments 5 --importance_weights 0.1 --algorithm optiongan --iterations 40 --env CartPole-v0 > opt_log 2> opt_error_log"
-$cmd
+#cmd="python -u run_training_gans.py experts/expert_rollouts_CartPole-v0.pickle novices/novice_policy_CartPole_gan_opt_f4.o --num_frames 4 --num_experiments 5 --importance_weights 0.1 --algorithm optiongan --iterations 40 --env CartPole-v0 > opt_log 2> opt_error_log"
+#eval $cmd
 
 # 4 frames importance .5
-cmd="python -u run_training_gans.py experts/expert_rollouts_CartPole-v0.pickle novices/novice_policy_CartPole_gan_opt_f4.o --num_frames 4 --num_experiments 5 --importance_weights 0.5 --algorithm optiongan --iterations 40 --env CartPole-v0 > opt_log 2> opt_error_log"
-disown -h $cmd &
+#cmd="python -u run_training_gans.py experts/expert_rollouts_CartPole-v0.pickle novices/novice_policy_CartPole_gan_opt_f4.o --num_frames 4 --num_experiments 5 --importance_weights 0.5 --algorithm optiongan --iterations 40 --env CartPole-v0 > opt_log 2> opt_error_log"
+#nohup  $cmd &
 
-cmd="python -u run_training_gans.py experts/expert_rollouts_CartPole-v0.pickle novices/novice_policy_CartPole_gan_mix_f4.o --num_frames 4 --num_experiments 5 --importance_weights 0.5 --algorithm mixgan --iterations 40 --env CartPole-v0 > mix_log 2> mix_error_log"
-$cmd
+#cmd="python -u run_training_gans.py experts/expert_rollouts_CartPole-v0.pickle novices/novice_policy_CartPole_gan_mix_f4.o --num_frames 4 --num_experiments 5 --importance_weights 0.5 --algorithm mixgan --iterations 40 --env CartPole-v0 > mix_log 2> mix_error_log"
+#eval $cmd
 
 # 4 frames importance 1.0
-cmd="python -u run_training_gans.py experts/expert_rollouts_CartPole-v0.pickle novices/novice_policy_CartPole_gan_opt_f4.o --num_frames 4 --num_experiments 5 --importance_weights 1.0 --algorithm optiongan --iterations 40 --env CartPole-v0 > opt_log 2> opt_error_log"
-disown -h $cmd &
+#cmd="python -u run_training_gans.py experts/expert_rollouts_CartPole-v0.pickle novices/novice_policy_CartPole_gan_opt_f4.o --num_frames 4 --num_experiments 5 --importance_weights 1.0 --algorithm optiongan --iterations 40 --env CartPole-v0 > opt_log 2> opt_error_log"
+#nohup  $cmd &
 
-cmd="python -u run_training_gans.py experts/expert_rollouts_CartPole-v0.pickle novices/novice_policy_CartPole_gan_mix_f4.o --num_frames 4 --num_experiments 5 --importance_weights 1.0 --algorithm mixgan --iterations 40 --env CartPole-v0 > mix_log 2> mix_error_log"
-$cmd
+#cmd="python -u run_training_gans.py experts/expert_rollouts_CartPole-v0.pickle novices/novice_policy_CartPole_gan_mix_f4.o --num_frames 4 --num_experiments 5 --importance_weights 1.0 --algorithm mixgan --iterations 40 --env CartPole-v0 > mix_log 2> mix_error_log"
+#eval $cmd
